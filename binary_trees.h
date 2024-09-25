43410706c360a2d38bc92f4486b9044794a0dd66
elkomysara <sa1244@fayoum.edu.eg>
2024-09-26 00:35:39 +0300

    Implement binary_tree_insert_right function and update header

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
 * struct binary_tree_s - Binary tree node
@@ -24,6 +25,7 @@ typedef struct binary_tree_s binary_tree_t;
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
