812f5caba6af6c0b12375736656017988bb9bf98
elkomysara <sa1244@fayoum.edu.eg>
2024-09-25 21:08:39 +0300

    Implement binary tree node creation function and test with main

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -5,14 +5,14 @@
 /* Helper function to print the tree */
 void binary_tree_print(const binary_tree_t *tree)
 {
-    if (!tree)
-        return;
+if (!tree)
+return;
 
-    if (tree->left)
-        printf("(%03d) left: (%03d)\n", tree->n, tree->left->n);
-    if (tree->right)
-        printf("(%03d) right: (%03d)\n", tree->n, tree->right->n);
+if (tree->left)
+printf("(%03d) left: (%03d)\n", tree->n, tree->left->n);
+if (tree->right)
+printf("(%03d) right: (%03d)\n", tree->n, tree->right->n);
 
-    binary_tree_print(tree->left);
-    binary_tree_print(tree->right);
+binary_tree_print(tree->left);
+binary_tree_print(tree->right);
 }
