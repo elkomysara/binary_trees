f1ab7eeb9fe98c4b4b539b0152ca0ba7080eff27
elkomysara <sa1244@fayoum.edu.eg>
2024-09-26 03:03:25 +0300

    Implement binary_tree_balance function and test

--- binary_trees.h
+++ binary_trees.h
@@ -35,7 +35,8 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
-size_t binary_tree_nodes(const binary_tree_t *tree); /* New function prototype */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree); /* New function prototype */
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
